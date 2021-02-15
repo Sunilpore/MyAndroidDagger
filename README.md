c295e338c7756ffbaaeb79f27696ff36b481d714
Sunil Pore <sunilyahoo95@gmail.com>
2021-02-15 17:26:20 +0530

    check hilt scope functions

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ References:-
 DaggerHilt official documentation:
 -https://dagger.dev/hilt/
 -https://developer.android.com/training/dependency-injection/hilt-android
+
+Android Scope Reference:-
+-https://developer.android.com/training/dependency-injection/hilt-android#component-scopes
+-https://medium.com/androiddevelopers/scoping-in-android-and-hilt-c2e5222317c0
--- app/src/main/java/com/daggerhilteg/MainActivity.kt
+++ app/src/main/java/com/daggerhilteg/MainActivity.kt
@@ -6,8 +6,12 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.fragment.app.Fragment
 import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.scopes.ActivityScoped
+import dagger.hilt.android.scopes.FragmentScoped
 import javax.inject.Inject
+import javax.inject.Singleton
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -23,10 +27,30 @@ class MainActivity : AppCompatActivity() {
         println(somClass.getAThing())
         println(somClass.getAnotherThing())
 
+        println("obj_loc: $somClass")
+
     }
 
 }
 
+@AndroidEntryPoint
+class MyFragment(): Fragment() {
+
+    @Inject
+    lateinit var somClass: SomClass
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        print("obj_loc2: $somClass")
+    }
+}
+
+
+
+@Singleton
+//@ActivityScoped
+//@FragmentScoped  //Note: You can' into activity level as mentioned dagger graph hierarchy
 class SomClass
 @Inject
 constructor(
